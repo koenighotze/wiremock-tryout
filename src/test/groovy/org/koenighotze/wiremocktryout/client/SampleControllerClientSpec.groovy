af14bad532b588ab0083c2c00b3a54158f31351e
David Schmitz <david.schmitz.privat@gmail.com>
2016-03-21 22:03:27 +0100

    Integrated spock testing

---
+++ src/test/groovy/org/koenighotze/wiremocktryout/client/SampleControllerClientSpec.groovy
@@ -0,0 +1,32 @@
+package org.koenighotze.wiremocktryout.client
+
+import org.koenighotze.wiremocktryout.service.SampleApplication
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.test.SpringApplicationConfiguration
+import spock.lang.Specification
+
+/**
+ * @author David Schmitz
+ */
+@SpringApplicationConfiguration(classes = SampleControllerClientApplication.class)
+class SampleControllerClientSpec extends Specification {
+
+    @Autowired
+    def SampleControllerClient sampleControllerClient
+
+    def "The Samples can be fetched using REST"() {
+        setup: "a running application"
+        def configurableApplicationContext = new SpringApplication(SampleApplication.class).run();
+
+        when: "we fetch the samples"
+        def result = sampleControllerClient.fetchAllSamples()
+
+        then: "we receive a non empty list"
+        result != null
+        result.size() > 0
+
+        cleanup:
+        configurableApplicationContext.close();
+    }
+}
---
+++ src/test/groovy/org/koenighotze/wiremocktryout/service/SampleControllerSpec.groovy
@@ -0,0 +1,54 @@
+package org.koenighotze.wiremocktryout.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.test.annotation.Rollback
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.web.context.WebApplicationContext
+import spock.lang.Specification
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup
+
+/**
+ * @author David Schmitz
+ */
+@SpringApplicationConfiguration(classes = SampleApplication.class)
+@WebAppConfiguration
+@Rollback
+class SampleControllerSpec extends Specification {
+    @Autowired
+    def WebApplicationContext webApplicationContext
+    def MockMvc mockMvc
+
+    def setup() {
+        mockMvc = webAppContextSetup(webApplicationContext).build()
+    }
+
+    def "a known sample can be GET"() {
+        given: "A sample with an id"
+
+        when: "we try to fetch a sample with that id"
+        def perform = mockMvc.perform(get("/sample/1"))
+
+        then: "we expect a successful JSON response"
+        perform.andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8));
+    }
+
+    def "The list of samples can be fetched via GET"() {
+        given: "A GET request to the controller URL"
+        def perform = mockMvc.perform(get("/sample/"));
+
+        when: "We read the returned JSON data"
+
+        then: "We expect a list of 11 elements"
+        perform.andExpect(status().isOk())
+               .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+               .andExpect(jsonPath('$', hasSize(11)));
+    }
+}
